bd36cf851eab495cf8a87e1cba3baec3e08360dc
Justin Kerntz and Nathan Aden <nathan@>
2019-04-23 08:11:50 -0700

    add inital files and MSTest

---
+++ LeapYearTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeapYear;
+
+namespace LeapYearTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
